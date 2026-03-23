ff802edbc89d6d90a57814adf238ec1f1587b9ae
ramires <ramiresdamazio@hotmail.com>
2026-03-23 17:57:19 -0300

    feito mais coisas

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { sequelize } from './config/database.js'
 import userRoutes from './routes.js'
+import { Message } from './models/message.js'
+import { User } from './models/user.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -26,10 +28,20 @@ await fastify.ready()
 
 fastify.io.on('connection', (socket) => {
     fastify.log.info(`✅ Novo usuário conectado com sucesso: ${socket.id}`)
+    socket.on('enviar_mensagem', async (dados) => {
+        const { userId, content } = dados
+        const mensagemSalva = await Message.create({
+            userId: '123',
+            content: 'teste'
+        })
+        fastify.io.emit('testando', mensagemSalva)
+    })
 })
 
 try {
     await sequelize.authenticate()
+    User.hasMany(Message, { foreignKey: 'userId' })
+    Message.belongsTo(User, { foreignKey: 'userId' })
     await sequelize.sync()
     fastify.log.info("Banco de dados conectado ao fastify/io")
     await fastify.listen({ port: 3000 })
