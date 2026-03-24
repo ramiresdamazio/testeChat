3e7e2677265c149020f851ee94e3d2174d4397fa
ramires <ramiresdamazio@hotmail.com>
2026-03-24 17:02:05 -0300

    chore: omit database credentials using .env

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,12 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize('chatdb', 'postgres', '1234', {
-    host: 'localhost',
-    dialect: 'postgres'
-})
+export const sequelize = new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASS, 
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres'
+    }
+);
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import Fastify from 'fastify'
 import fastifyStatic from '@fastify/static'
 import fastifySocketIo from '@wick_studio/fastify-socket.io'
